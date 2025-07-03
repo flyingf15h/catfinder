706aa7370d3ad2b8fbaeef39304aabd582a4b682
axolotl <109440858+flyingf15h@users.noreply.github.com>
2025-07-03 12:25:57 -0400

    Add files via upload

--- catfinder.ino
+++ catfinder.ino
@@ -1,148 +1,248 @@
-#include "Keyboard.h"
+#include <Arduino.h>
 
-const int LED1 = 26;  // dot
-const int LED2 = 27;  // dash
-const int BUT1 = 0;
-const int BUT2 = 1;
-const int BUT3 = 2;
-const int BUT4 = 3;
+const int BUTTON_PINS[] = {2, 3, 4, 5}; 
+const int LED_PINS[] = {9, 10, 11};  
 
-bool LED1locked = false;
-bool LED2locked = false;
-bool LED1TempState = false;
-bool LED2TempState = false;
+enum GameState { IDLE, STARTING, PLAYING, ENDING, SHOW_SCORE };
+GameState gameState = IDLE;
 
-const unsigned long dD = 20;
-const unsigned long kbCd = 600;
+// Game variables
+unsigned long startTime = 0; 
+unsigned long lastFlashTime = 0;
+int currentSpeed = 1;  
+const int SPEED_DELAYS[] = {500, 350, 200};
+int score = 0;
+bool ledsActive[3] = {false, false, false};
+unsigned long ledStartTimes[3] = {0, 0, 0};
+int targetBrightness[3] = {0, 0, 0};
+int totalBursts = 0; // max is 30                  
+unsigned long nextBurst = 0; 
 
-unsigned long lastchange1 = 0;
-unsigned long lastchange2 = 0;
-unsigned long lastchange3 = 0;
-unsigned long lastchange4 = 0;
-unsigned long BUT1cd = 0;
-unsigned long BUT2cd = 0;
-unsigned long BUT4cd = 0;
-
-bool laststate1 = HIGH;
-bool laststate2 = HIGH;
-bool laststate3 = HIGH;
-bool laststate4 = HIGH;
+// Score 
+int currentScoreDigit = 0;
+int digitValues[3] = {0, 0, 0};
+unsigned long lastDigitChange = 0;
 
 void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(BUT1, INPUT_PULLUP);
-  pinMode(BUT2, INPUT_PULLUP);
-  pinMode(BUT3, INPUT_PULLUP);
-  pinMode(BUT4, INPUT_PULLUP);
-
-  Keyboard.begin();
+  for (int i = 0; i < 4; i++) {
+    pinMode(BUTTON_PINS[i], INPUT_PULLUP);
+  }
+  
+  for (int i = 0; i < 3; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+    analogWrite(LED_PINS[i], 0);
+  }
+  
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
 }
 
 void loop() {
-  unsigned long currTime = millis();
-
-  bool sw1 = digitalRead(BUT1) == LOW;
-  bool sw2 = digitalRead(BUT2) == LOW;
-  bool sw3 = digitalRead(BUT3) == LOW;
-  bool sw4 = digitalRead(BUT4) == LOW;
-
-  // Button 1 
-  if (sw1 != laststate1) {
-    lastchange1 = currTime;
+  unsigned long currentTime = millis();
+  static unsigned long but4time = 0;
+  static bool button4Held = false;
+  
+  bool but4 = digitalRead(BUTTON_PINS[3]) == LOW;
+  
+  if (but4 && !button4Held) {
+    but4time = currentTime;
+    button4Held = true;
+  } else if (!but4 && button4Held) {
+    button4Held = false;
+    if (gameState == IDLE && currentTime - but4time < 2000) {
+      currentSpeed = (currentSpeed + 1) % 3;
+      showSpeedIndicator();
+    } else if (currentTime - but4time >= 2000) {
+      toggleGame();
+    }
   }
-  if ((currTime - lastchange1) > dD) {
-    if (sw1 != laststate1) {
-      laststate1 = sw1;
-      if (sw1 && (currTime - BUT1cd >= kbCd)) { 
-        BUT1cd = currTime;
-        if (!LED1locked) {
-          LED1TempState = true;
+
+  // Game state machine
+  switch (gameState) {
+    case IDLE:
+      setBrightness(0);
+      break;
+      
+    case STARTING:
+      if (currentTime - startTime < 3000) {
+        if (currentTime - lastFlashTime > 500) {
+          toggleLEDs();
+          lastFlashTime = currentTime;
         }
-        Keyboard.print("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧");
+      } else {
+        gameState = PLAYING;
+        score = 0; 
+        startNewRound();
       }
-    }
-  }
-  if (!sw1) {
-    LED1TempState = false;
-  }
+      break;
+      
+    case PLAYING:
+      updateLEDs(currentTime);    
+      checkButtons(currentTime);   
 
-  // Button 2  
-  if (sw2 != laststate2) {
-    lastchange2 = currTime;
-  }
-  if ((currTime - lastchange2) > dD) {
-    if (sw2 != laststate2) {
-      laststate2 = sw2;
-      if (sw2 && (currTime - BUT2cd >= kbCd)) {
-        BUT2cd = currTime;
-        if (!LED2locked) {
-          LED2TempState = true;
+      if (currentTime >= nextBurst && totalBursts < 30) {
+        ledBurst(currentTime);
+        totalBursts++;
+
+        int pause = random(100, 451);
+        nextBurst = currentTime + pause;
+      }
+
+      if (totalBursts >= 30) {
+        gameState = ENDING;
+        startTime = currentTime;
+      }
+      break;
+
+    case ENDING:
+      if (currentTime - startTime < 3000) {
+        if (currentTime - lastFlashTime > 500) {
+          toggleLEDs();
+          lastFlashTime = currentTime;
         }
-        Keyboard.print("(^◕.◕^)");
+      } else {
+        prepScore();
+        gameState = SHOW_SCORE;
       }
-    }
+      break;
+      
+    case SHOW_SCORE:
+      displayScore(currentTime);
+      break;
   }
-  if (!sw2) {
-    LED2TempState = false;
-  }
-
-  // Button 3 
-  if (sw3 != laststate3) {
-    lastchange3 = currTime;
-  }
-  if ((currTime - lastchange3) > dD) {
-    if (sw3 != laststate3) {
-      laststate3 = sw3;
-      if (sw3) {  
-        LED1locked = !LED1locked;
-        LED2locked = !LED2locked;
-      }
-    }
-  }
-
-  // Button 4 
-  if (sw4 != laststate4) {
-    lastchange4 = currTime;
-  }
-  if ((currTime - lastchange4) > dD) {
-    if (sw4 != laststate4) {
-      laststate4 = sw4;
-      if (sw4 && (currTime - BUT4cd >= kbCd)) {
-        BUT4cd = currTime;
-        Keyboard.press(KEY_BACKSPACE);
-        Keyboard.release(KEY_BACKSPACE);
-        playMorse("..-...-......");
-      }
-    }
-  }
-
-  digitalWrite(LED1, LED1locked || LED1TempState);
-  digitalWrite(LED2, LED2locked || LED2TempState);
 }
 
-void playMorse(const char* morse) {
-  for (int i = 0; morse[i] != '\0'; i++) {
-    char c = morse[i];
+void toggleGame() {
+  if (gameState == IDLE) {
+    gameState = STARTING;
+    startTime = millis();
+  } else if (gameState == PLAYING) {
+    gameState = ENDING;
+    startTime = millis();
+  }
+}
 
-    if (c == '.') {
-      digitalWrite(LED1, HIGH);
-      delay(200);
-      digitalWrite(LED1, LOW);
-    } else if (c == '-') {
-      digitalWrite(LED2, HIGH);
-      delay(400);
-      digitalWrite(LED2, LOW);
+void showSpeedIndicator() {
+  for (int i = 0; i < 3; i++) {
+    for (int brightness = 0; brightness <= 255; brightness += 5) {
+      analogWrite(LED_PINS[i], brightness);
+      delay(10);
     }
+    for (int brightness = 255; brightness >= 0; brightness -= 5) {
+      analogWrite(LED_PINS[i], brightness);
+      delay(10);
+    }
+  }
+}
 
-    delay(200);
-    if (digitalRead(BUT1) == LOW || digitalRead(BUT2) == LOW || 
-        digitalRead(BUT3) == LOW || digitalRead(BUT4) == LOW) {
-      return;
-    }
+void setBrightness(int brightness) {
+  for (int i = 0; i < 3; i++) {
+    analogWrite(LED_PINS[i], brightness);
+  }
+}
 
-    if (i == 3 || i == 5 || i == 8) {
-      delay(400);
+void toggleLEDs() { 
+  for (int i = 0; i < 3; i++) {
+    int current = analogRead(LED_PINS[i]) > 128 ? 255 : 0;
+    int newVal = current ? 0 : 255;
+    analogWrite(LED_PINS[i], newVal);
+    ledsActive[i] = (newVal > 0);
+  }
+}
+
+void ledBurst(unsigned long currentTime) {
+  int numLEDs = random(1, 4);  
+  bool selected[3] = {false, false, false};
+
+  for (int i = 0; i < numLEDs; ) {
+    int led = random(0, 3);
+    if (!selected[led]) {
+      selected[led] = true;
+      ledsActive[led] = true;
+      ledStartTimes[led] = currentTime;
+      targetBrightness[led] = 255;
+      analogWrite(LED_PINS[led], 0);
+      i++;
     }
+  }
+}
+
+void startNewRound() {
+  for (int i = 0; i < 3; i++) {
+    ledsActive[i] = false;
+    analogWrite(LED_PINS[i], 0);
+  }
+  totalBursts = 0;
+  nextBurst = millis();
+}
+
+void updateLEDs(unsigned long currentTime) {
+  for (int i = 0; i < 3; i++) {
+    if (ledsActive[i]) {
+      unsigned long elapsed = currentTime - ledStartTimes[i];
+      int rampTime = SPEED_DELAYS[currentSpeed];
+
+      if (elapsed < rampTime) {
+        int brightness = map(elapsed, 0, rampTime, 0, 255);
+        analogWrite(LED_PINS[i], brightness);
+      } else if (elapsed < rampTime + 100) {
+        analogWrite(LED_PINS[i], 255);
+      } else {
+        analogWrite(LED_PINS[i], 0);
+        ledsActive[i] = false;
+      }
+    }
+  }
+}
+
+void checkButtons(unsigned long currentTime) {
+  for (int i = 0; i < 3; i++) {
+    if (ledsActive[i] && digitalRead(BUTTON_PINS[i]) == LOW) {
+      unsigned long elapsed = currentTime - ledStartTimes[i];
+      int rampTime = SPEED_DELAYS[currentSpeed];
+      int timeDiff = abs(elapsed - rampTime);
+
+      if (timeDiff <= 40) {
+        score += 2;
+        analogWrite(LED_PINS[i], 0);
+        ledsActive[i] = false;
+      } else if (timeDiff <= 80) {
+        score += 1;
+        analogWrite(LED_PINS[i], 0);
+        ledsActive[i] = false;
+      }
+    }
+  }
+}
+
+void prepScore() {
+  digitValues[0] = (score / 100) % 10; 
+  digitValues[1] = (score / 10) % 10;  
+  digitValues[2] = score % 10;        
+  currentScoreDigit = 0;
+  lastDigitChange = millis();
+}
+
+void displayScore(unsigned long currentTime) {
+  if (currentTime - lastDigitChange > 1000) {
+    currentScoreDigit = (currentScoreDigit + 1) % 3;
+    lastDigitChange = currentTime;
+    setBrightness(0);
+  }
+
+  if (currentScoreDigit < 3) {
+    unsigned long flashTime = (currentTime - lastDigitChange) % 500;
+    int flashes = digitValues[currentScoreDigit];
+
+    if (flashes > 0) {
+      int flashState = (flashTime / (500 / (flashes * 2))) % 2;
+      analogWrite(LED_PINS[currentScoreDigit], flashState ? 255 : 0);
+    } else {
+      analogWrite(LED_PINS[currentScoreDigit], 0);
+    }
+  }
+
+  if (currentScoreDigit == 0 && currentTime - lastDigitChange > 1000) {
+    gameState = IDLE;
   }
 }
